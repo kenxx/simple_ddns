daa0079f77111feef58bffee7a23f79684c5caf2
Kenneth <kenneth@influx.io>
2022-09-06 16:49:47 +0800

    initial

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"bytes"
+	"crypto/sha256"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"github.com/labstack/echo/v4"
+	"gopkg.in/ini.v1"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"path"
 )
 
 type Response struct {
@@ -20,36 +24,62 @@ type PutIp struct {
 	Target string `json:"target"`
 }
 
-func main() {
-	token := os.Getenv("LINODE_API_TOKEN")
-	domainId := os.Getenv("LINODE_DOMAIN_ID")
-	recordId := os.Getenv("LINODE_DOMAIN_RECORD_ID")
+type DDNS struct {
+	Type           string `ini:"type"`
+	APIToken       string `ini:"api_token"`
+	DomainID       int    `ini:"domain_id"`
+	DomainRecordID int    `ini:"domain_record_id"`
+}
 
-	url := fmt.Sprintf("https://api.linode.com/v4/domains/%s/records/%s", domainId, recordId)
+func main() {
+	wd, _ := os.Getwd()
+	configPath := path.Join(wd, "ddns.ini")
+	log.Printf("configPath %s", configPath)
+	load, err := ini.Load(configPath)
+	if err != nil {
+		panic(err)
+	}
+	conf := new(DDNS)
+	if err = load.MapTo(&conf); err != nil {
+		panic(err)
+	}
+
+	url := fmt.Sprintf("https://api.linode.com/v4/domains/%d/records/%d", conf.DomainID, conf.DomainRecordID)
+
+	p := fmt.Sprintf("%d:%d:%s", conf.DomainID, conf.DomainRecordID, conf.APIToken)
+	s := sha256.Sum256([]byte(p))
+	sid := hex.EncodeToString(s[:])
+
+	log.Printf("id bind %s", sid)
 
 	e := echo.New()
+	e.GET("/update/:id", func(c echo.Context) error {
+		id := c.Param("id")
+		if id != sid {
+			return c.JSON(http.StatusBadRequest, &Response{-1, "id not fit"})
+		}
 
-	cli := http.Client{}
-
-	e.GET("/update", func(c echo.Context) error {
 		b, _ := json.Marshal(&PutIp{Target: c.RealIP()})
-		request, err := http.NewRequest(http.MethodPut, url, bytes.NewReader(b))
+		request, err := http.NewRequest(http.MethodPut, url, bytes.NewBuffer(b))
 		if err != nil {
 			return c.JSON(http.StatusInternalServerError, &Response{1, err.Error()})
 		}
 
 		log.Printf("try to set ip=%s", c.RealIP())
-		request.Header.Set("authorization", "Bearer "+token)
+		request.Header.Set("Content-Type", "application/json")
+		request.Header.Set("Authorization", "Bearer "+conf.APIToken)
 
-		response, err := cli.Do(request)
+		response, err := http.DefaultClient.Do(request)
 		if err != nil {
 			return c.JSON(http.StatusInternalServerError, &Response{2, err.Error()})
 		}
 
+		r, _ := io.ReadAll(response.Body)
 		if response.StatusCode != http.StatusOK {
-			r, _ := io.ReadAll(response.Body)
 			return c.JSON(http.StatusInternalServerError, &Response{3, string(r)})
 		}
+
+		log.Printf("dns provider response: %s", r)
 
 		return c.JSON(http.StatusOK, &Response{0, "ok"})
 	})
